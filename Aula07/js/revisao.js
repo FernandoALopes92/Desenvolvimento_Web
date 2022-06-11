5ca9f27f0a2e1088555844e6264f193bf53f998e
FernandoALopes92 <fernando.lopes_92@hotmail.com>
2022-06-11 10:36:04 -0300

    Adicionando arquivo de revisão referente a aula 06

---
+++ Aula07/js/revisao.js
@@ -0,0 +1,12 @@
+var listaPessoasPresentes = []
+
+var aluno1 = {
+    nome: 'Fulano',
+    idade: 11
+}
+
+if(aluno1.idade >= 12){
+    console.log('Pode escrever de caneta')
+} else {
+    console.log('Pode escrever de caneta')
+}
